d8c0eb2ac3c890aa27496a8e804453e2cd5b6968
Dayna Leal <dayna.r.leal@gmail.com>
2020-08-18 20:30:54 -0500

    completed variables and data types

--- Program.cs
+++ Program.cs
@@ -9,12 +9,30 @@ namespace csharp
             // comments are written like this
             /* or like
             this */
-            Console.WriteLine("\nWhat is your name? ");
+            // Console.WriteLine("\nWhat is your name? ");
             var name = Console.ReadLine();
             var date = DateTime.Now;
-            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
-            Console.Write("\nPress any key to exit...");
-            Console.ReadKey(true);
+            // Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            // Console.Write("\nPress any key to exit...");
+            // Console.ReadKey(true);
+
+            // Create Variables
+            string dogName = "Shadow";
+            string breed = "Golden Retriever";
+            int age = 5;
+            double weight = 65.22;
+            bool spayed = true;
+
+            // Print variables to the console
+            Console.WriteLine(dogName);
+            Console.WriteLine(breed);
+            Console.WriteLine(age);
+            Console.WriteLine(weight);
+            Console.WriteLine(spayed);
+
+            Console.Write("Pick a number: ");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"You chose the number {numInput} on {date:d} at {date:t}\n");
         }
     }
 }
