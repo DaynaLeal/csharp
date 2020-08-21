6045a261e6ef8371f48a0cc63e28976d9f7d2182
Dayna Leal <dayna.r.leal@gmail.com>
2020-08-20 23:00:47 -0500

    completed bool and operators lesson

--- Program.cs
+++ Program.cs
@@ -159,6 +159,23 @@ namespace csharp
             sceneDescription = sceneDescription.ToLower();
             // Print results
             Console.WriteLine($"{cameraDirections} {sceneDescription}");
+
+            double timeToDinner = 4;
+            double distance = 95;
+            double rate = 30;
+            double tripDuration = distance / rate;
+            bool answer = tripDuration <= timeToDinner;
+            Console.WriteLine(answer);
+
+            bool beach = true;
+            bool hiking = false;
+            bool city = true;
+            bool yourNeeds = beach && city;
+            bool friendNeeds = beach || hiking;
+            bool tripDecision = yourNeeds && friendNeeds;
+            Console.WriteLine(tripDecision);
+
+            
         }
     }
 }
